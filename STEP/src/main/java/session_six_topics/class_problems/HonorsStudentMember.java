322ce01bbcfde576fdb4c8d561ce95d017bb495a
SV-Shrawan <srirangamshrawan@gmail.com>
2026-09-21 00:36:39 +0530

    Add Week 6 class problems

---
+++ STEP/src/main/java/session_six_topics/class_problems/HonorsStudentMember.java
@@ -0,0 +1,15 @@
+package session_six_topics.class_problems;
+
+public class HonorsStudentMember extends StudentMember {
+    private int bonusLimit;
+
+    public HonorsStudentMember(String memberId, int borrowLimit, String course, int bonusLimit) {
+        super(memberId, borrowLimit, course);
+        this.bonusLimit = bonusLimit;
+    }
+
+    @Override
+    public String displayInfo() {
+        return "Honors Student Member | Course: " + getCourse() + " | Bonus Limit: " + bonusLimit + " | Books Borrowed: " + getBooksBorrowed();
+    }
+}
---
+++ STEP/src/main/java/session_six_topics/class_problems/StudentMember.java
@@ -0,0 +1,24 @@
+package session_six_topics.class_problems;
+
+public class StudentMember extends LibraryMember {
+    private String course;
+
+    public StudentMember(String memberId, int borrowLimit, String course) {
+        super(memberId, borrowLimit);
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    @Override
+    public String displayInfo() {
+        return "Student Member | Course: " + course + " | Books Borrowed: " + getBooksBorrowed();
+    }
+
+    @Override
+    protected void chargeFine(int amount) {
+        super.chargeFine(amount / 2);
+    }
+}
